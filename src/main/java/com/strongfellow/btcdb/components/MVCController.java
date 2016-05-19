a60c9e2bcab863ebb66017025a286ad3eece2a72
strongfellow <a@b.com>
2016-05-18 20:17:00 -0700

    basic working template

--- src/main/java/com/strongfellow/btcdb/components/MVCController.java
+++ src/main/java/com/strongfellow/btcdb/components/MVCController.java
@@ -1,16 +1,20 @@
 package com.strongfellow.btcdb.components;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MVCController {
 
-    @RequestMapping(path="/api/block", method=RequestMethod.GET, produces="text/html")
-    public String welcome(Map<String, Object> model) {
-        return "block";
+    @RequestMapping(value="/api/block/{blockHash}/summary.html", method=RequestMethod.GET)
+    public String blockSummary(
+            @PathVariable("blockHash") String blockHash,
+            @ModelAttribute("model") ModelMap model) {
+        model.addAttribute("blockHash", blockHash);
+        return "block-summary";
     }
 }
--- src/main/resources/templates/block-summary.ftl
+++ src/main/resources/templates/block-summary.ftl
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+  <title>Strongfellow - Block ${model.blockHash}</title>
+</head>
 <body>
 block.ftl is live
 </body>
